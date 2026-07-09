a2421d60a85509fbb61fd4a6d112ecb4686b0c3a
Ali Mohamed <alimohamed351@outlook.com>
2026-07-10 00:42:48 +0300

    added authorization middleware

--- src/middlewares/authenticate.ts
+++ src/middlewares/authenticate.ts
@@ -3,20 +3,24 @@ import { verifyToken } from "../utilities/jwt/jwt.ts";
 import logger from "../utilities/logger/winston.ts";
 
 export function auth(req: Request, res: Response, next: NextFunction) {
-  const token =
+  const accessToken =
     req.cookies?.accessToken ??
     (req.headers.authorization?.startsWith("Bearer ")
       ? req.headers.authorization.slice(7)
       : undefined);
 
-  if (!token) {
+  // const refreshToken = req.cookies?.refreshToken ?? (req.headers.authorization?.startsWith("Bearer ")
+  //   ? req.headers.authorization.slice(7)
+  //   : undefined);
+
+  if (!accessToken) {
     return res
       .status(401)
       .json({ success: false, message: "Authentication required!" });
   }
 
   try {
-    req.user = verifyToken(token);
+    req.user = verifyToken(accessToken);
     next();
   } catch (error: any) {
     logger.warn(`authenticate failed: ${error.message}`);
